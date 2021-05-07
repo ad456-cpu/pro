cd2240be9d673482c027a2487d31dda6332454e2
ad456-cpu <76479763+ad456-cpu@users.noreply.github.com>
2021-05-07 20:43:58 +0530

    Add files via upload

---
+++ js/launcher.js
@@ -0,0 +1,26 @@
+class launcher{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stifness: 0.01,
+            length: 10
+        }
+        this.pointB=pointB;
+        this.launcher = Constraint.create(options);
+        World.add(world ,this.launcher);
+    }
+    fly(){
+        this.launcher.bodyA=null;
+    }
+
+    display(){
+		if(this.launcher.bodyA){
+			var pointA=this.bodyA.position;
+			var pointB=this.pointB
+
+			strokeWeight(2);		
+			line(pointA.x,pointA.y,pointB.x,pointB.y);
+		}}
+}
+
